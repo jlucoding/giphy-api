cf9b2b341553b4696e5143275628fd07be7a0b98
Alex Shevlin <alexshevlin1@gmail.com>
2022-06-27 14:02:28 -0700

    add inputs for keyword and limits and create XMLHttpRequest() on click for giphy API.
    Co-authored-by: Jonathan Lu <jdlu.coding@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,4 +2,32 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Template from './js/template.js';
+
+$(document).ready(function() {
+  $('#gifKeyword').click(function() {
+    const keyword = $('#keyword').val();
+    const limit = $('#pageLimit').val();
+    $('#keyword').val("");
+    $('#pageLimit').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${keyword}&limit=${limit}&offset=0&rating=g&lang=en`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      } else if (this.readyState === 4 && this.status === 404) {
+        $('.showErrors').text(`Please enter another keyword.`)
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      $('.showGif').html(`<img `)
+    }
+
+  });
+});
