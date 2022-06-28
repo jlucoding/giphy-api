870db3c8a5e13e3584d47aaba4a1a1d12c12b041
Alex Shevlin <alexshevlin1@gmail.com>
2022-06-27 17:00:16 -0700

    allow users to select how many responses by inputing a limit on the page.
    Co-authored-by: Jonathan Lu <jlucoding@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -7,17 +7,19 @@ import './css/styles.css';
 $(document).ready(function() {
   $('#gifKeyword').click(function() {
     const keyword = $('#keyword').val();
-    const limit = $('#pageLimit').val();
+    const limit = $('#limit').val();
+    const limitInt = parseInt(limit);
+    console.log(limitInt);
     $('#keyword').val("");
-    $('#pageLimit').val("");
+    $('#limit').val("");
 
     let request = new XMLHttpRequest();
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${keyword}&limit=${limit}&offset=0&rating=g&lang=en`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${keyword}&limit=${limit}`;
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
-        getElements(response, limit);
+        getElements(response, limitInt);
       } else if (this.readyState === 4 && this.status === 404) {
         $('.showErrors').text(`Please enter another keyword.`);
       }
@@ -26,14 +28,45 @@ $(document).ready(function() {
     request.open("GET", url, true);
     request.send();
 
-    function getElements(response, limit) {
-      if (limit === 1) {
-        $('.showGif').html(`<a href=${response.data[0].url} target="_blank">${keyword} 1</a>`);
-      } else if (limit < 1) {
-        let limitArray = [];
-        limitArray.from({length: limit}.keys());
+    function getElements(response, limitInt) {
+      let limitArray = [];
+      let arrayIndex = limitArray.indexOf(limitInt);
+      
+      for (let i = 0; i <= limitInt; i++) {
+        limitArray[i] = i;
       }
+        console.log(limitArray);
+      limitArray.forEach(function(arrayIndex) {
+        let htmlIndex = (arrayIndex + 1);
+        $('.showGif').append(`<a href=${response.data[arrayIndex].url} target="_blank">${keyword} ${htmlIndex} </a>`);
+      });
     }
+      // if (limitInt === 1) 
+      // {
+      //   $('.showGif').append(`<a href=${response.data[getIndex(limitInt)].url} target="_blank">${keyword} ${limit}</a>`);
+      // } 
+      // else if (limitInt > 1) 
+      // {
+      //   let limitArray = [];
+      //   for (let i = 0; i <= limitInt; i++) {
+      //     limitArray[i] = i;
 
+      //   }
+      //   console.log(limitArray);
+      //   for (let i = 0; i < limitArray.length; i++) {
+      //     let newIndex = limitArray[i];
+      //     $('.showGif').append(`<a href=${response.data[getIndex(newIndex)].url} target="_blank">${keyword} ${newIndex} </a>`);
+      //   }
+
+        //create div with class for each data[i];
+        //".showGif(limitInt)".html(`<a href=${response.data[getIndex(limitInt)].url} target="_blank">${keyword} ${limit}</a>`);
+      // } else if (isNaN(limitInt)) {
+      //   console.log('please put in a number!');
+      // }
+
+    // function getIndex(limitInt) {
+    //   let index = (limitInt - 1);
+    //   return index;
+    // }
   });
 });
